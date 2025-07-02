96a8ddc33d7f2edb4f12a008c2c2aa8cd18ad926
Sinhaza <sinhazaruptura@gmail.com>
2025-07-02 18:05:11 +0200

    Created PasswordFactory

--- database/factories/PasswordFactory.php
+++ database/factories/PasswordFactory.php
@@ -2,22 +2,25 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Password>
  */
 class PasswordFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = \App\Models\Password::class;
+
     public function definition(): array
     {
         return [
-            //
+            'uuid' => (string) Str::uuid(),
+            'password' => 'secret-password',
+            'expires_at' => Carbon::now()->addHours(2),
+            'visit_limit' => $this->faker->numberBetween(1, 5),
+            'visit_count' => 0,
         ];
     }
 }
